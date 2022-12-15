7397bc9dd885d02c94772750d39ed7c0ff889c96
gracegoh <gracegoh924@gmail.com>
2022-12-15 17:15:56 +0900

    Deploy frontend on netlify

--- js/api.js
+++ js/api.js
@@ -1,6 +1,6 @@
 const backend_base_url = 'http://127.0.0.1:8000'
-const frontend_base_url = 'http://127.0.0.1:5500/html/'
-// const frontend_base_url = 'https://auto-colorshop.netlify.app/html/'
+// const frontend_base_url = 'http://127.0.0.1:5500/html/'
+const frontend_base_url = 'https://auto-colorshop.netlify.app/html/'
 
 // 로그인
 async function handleLogin() {
